1e94e2d07cec5fe60df210dfe4e4c5b665b37805
Vandivier <vandivier_john@yahoo.com>
2019-04-01 18:57:34 -0400

    fsGetNewSurveyMonkeyFileName

--- papers/alt-ed-survey/190201-feb-survey-monkey/data/wrangle/wrangler.js
+++ papers/alt-ed-survey/190201-feb-survey-monkey/data/wrangle/wrangler.js
@@ -22,9 +22,33 @@ function fGetInputFileLocations() {
         .map(sSubfolder => path.join(sSubfolder, ...arrsSurveyMonkeyCsvConventionalLocation));
 }
 
+const fpWrangleSurveyMonkeyFile = async sLocation => {
+    const sNewFileName = fsGetNewSurveyMonkeyFileName(sLocation);
+    const sOriginalFileContent = await fpReadFile(sLocation);
+    const sOutputFileContent = fsTransformSurveyMonkeyCsvContent(sOriginalFileContent);
+    const sOutputFileLocation = path.join(__dirname, sNewFileName);
+  
+    return fpWriteFile(sOutputFileLocation, sOutputFileContent, 'utf8');
+  };
+
+const fsGetNewSurveyMonkeyFileName = sLocation => {
+    const arrsPath = sLocation.split(path.sep);
+    const sDataFolderName = arrsPath[arrsPath.length - 3];
+
+    return (sDataFolderName + ' ' + path.basename(sLocation)).toLowerCase().replace(/[^\w.]/g, '-');
+}
+
+function fsTransformSurveyMonkeyCsvContent(sOriginalFileContent) {
+    const sNewFileContent = sOriginalFileContent;
+    // TODO
+    return sNewFileContent;
+}
+
 async function main() {
     const arrsInputFileLocations = fGetInputFileLocations();
-    debugger
+    const arrpFileOperations = arrsInputFileLocations.map(fpWrangleSurveyMonkeyFile)
+    await Promise.all(arrpFileOperations);
 }
 
 main();
+
