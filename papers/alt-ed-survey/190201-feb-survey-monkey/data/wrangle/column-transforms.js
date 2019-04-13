75563607402acf8aca186b2b37a25e3bc52a5502
Vandivier <vandivier_john@yahoo.com>
2019-04-12 21:03:49 -0400

    more columns and better column labels

--- papers/alt-ed-survey/190201-feb-survey-monkey/data/wrangle/column-transforms.js
+++ papers/alt-ed-survey/190201-feb-survey-monkey/data/wrangle/column-transforms.js
@@ -21,17 +21,45 @@ module.exports = [
     },
   },
   {
+    sMatcher: 'Do you work in a STEM profession?',
+    farroTransformer: function(sCellValue, arroTransformersWithIndex) {
+      const oTransformerStem = arroTransformersWithIndex.find(oTransformer => oTransformer.sOutputColumnName === 'IsStem');
+      const oTransformerUnsureStem = arroTransformersWithIndex.find(oTransformer => oTransformer.sOutputColumnName === 'IsUnsureStem');
+
+      return [
+        Object.assign({}, oTransformerStem, { value: sCellValue === '1' ? 1 : 0 }),
+        Object.assign({}, oTransformerUnsureStem, { value: sCellValue === '3' ? 1 : 0 }),
+      ];
+    },
+  },
+  {
     sMatcher: 'alternative credentials can qualify a person for an entry-level position',
+    sOutputColumnName: 'FavorEntryLevel',
   },
   {
     sMatcher:
       'It will soon become fairly conventional for high school graduates to obtain alternative credentials instead of going to college.',
+      sOutputColumnName: 'FavorConventionalSoon',
   },
   {
     sMatcher: 'When you add up the pros and cons for online education',
+    sOutputColumnName: 'FavorOnlineEducation',
+  },
+  {
+    sMatcher: 'When you add up the pros and cons for cryptocurrency',
+    sOutputColumnName: 'FavorCrypto',
   },
   {
     sMatcher: 'When you add up the pros and cons for artificial intelligence',
+    sOutputColumnName: 'FavorAI',
+  },
+  {
+    sMatcher: 'When you add up the pros and cons for artificial intelligence',
+    sOutputColumnName: 'FavorAI',
+  },
+  {
+    sMatcher: 'is important to check whether the degree was awarded from a US institution',
+    sOutputColumnName: 'FavorAmerican',
   },
   {
     sMatcher: 'Udacity',
@@ -50,37 +78,54 @@ module.exports = [
   },
   {
     sMatcher: 'Which of these industries most closely matches your profession?',
+    sOutputColumnName: 'Industry',
   },
   {
     sMatcher: 'businesses treat individuals more fairly',
+    sOutputColumnName: 'FavorRegulation',
+  },
+  {
+    sMatcher: 'I consider myself religious',
+    sOutputColumnName: 'FavorReligion',
+  },
+  {
+    sMatcher: 'I consider myself Christian',
+    sOutputColumnName: 'FavorChristianity',
   },
   {
     bExactMatch: true,
     sMatcher: 'Gender?',
+    sOutputColumnName: 'ReportedGender',
   },
   {
     bExactMatch: true,
     sMatcher: 'Household Income?',
+    sOutputColumnName: 'ReportedIncome',
   },
   {
     bExactMatch: true,
     sMatcher: 'Age?',
+    sOutputColumnName: 'ReportedAge',
   },
   {
     bExactMatch: true,
     sMatcher: 'Age',
+    sOutputColumnName: 'SurveyMonkeyAge',
   },
   {
     bExactMatch: true,
     sMatcher: 'Gender',
+    sOutputColumnName: 'SurveyMonkeyGender',
   },
   {
     bExactMatch: true,
     sMatcher: 'Household Income',
+    sOutputColumnName: 'SurveyMonkeyIncome',
   },
   {
     bExactMatch: true,
     sMatcher: 'Region',
+    sOutputColumnName: 'SurveyMonkeyRegion',
   },
   {
     bGeneratedColumn: true,
@@ -90,4 +135,12 @@ module.exports = [
     bGeneratedColumn: true,
     sOutputColumnName: 'IsUnemployed',
   },
+  {
+    bGeneratedColumn: true,
+    sOutputColumnName: 'IsStem',
+  },
+  {
+    bGeneratedColumn: true,
+    sOutputColumnName: 'IsUnsureStem',
+  }
 ];
