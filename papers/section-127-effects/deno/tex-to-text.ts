c4c3988e2ca2bcddaaebc21b2bbdde65b6f48c12
John Vandivier <john@afterecon.com>
2020-04-05 02:11:10 -0400

    tex-to-text done

--- papers/section-127-effects/deno/tex-to-text.ts
+++ papers/section-127-effects/deno/tex-to-text.ts
@@ -9,13 +9,52 @@
 // using windows 10
 // using WSL2 with Ubuntu 16.04.6 LTS
 //
+// project generated with `ameerthehacker.deno-vscode` + `deno init`
+//
 // ref: https://stackoverflow.com/questions/52905844/git-bash-on-windows-vs-wsl
-// https://deno.land/
+// ref: https://deno.land/
+//
+// TODO: secure command + Deno 1.0
+// OS-normalized EOL once supported: https://github.com/denoland/deno/issues/4631
+// TODO: follow up on GitHub issues in this article https://www.afterecon.com/programming/deno-grammarly-and-windows/
 
-const decoder = new TextDecoder("utf-8");
-const encoder = new TextEncoder()
-const sIn = await Deno.readFile("tsconfig.json");
-const sOut = decoder.decode(sIn).replace(/{/g, '!!!!!!!!!!!!!!!')
+import { BufReader, ReadLineResult } from "https://deno.land/std/io/bufio.ts";
 
+const decoder = new TextDecoder();
+const encoder = new TextEncoder();
 
+const handleLine = (sLine: string): string => {
+    const isComment = sLine.trim()[0] === "%";
+    const isOmittedFromTextVersion = sLine.includes('%%%'); // a convention
+    const isTechnical = sLine.trim()[0] === "\\" || sLine.trim()[0] === "}";
+    const regexMatchIsSectionHeading = sLine.match(/\\[a-z]*section{(?<headingText>[A-z- ]*)}/);
+
+    if (!isComment && !isTechnical && !isOmittedFromTextVersion) {
+        return sLine.trim() + '\n';
+    } else if(regexMatchIsSectionHeading) {
+        const headingText = regexMatchIsSectionHeading.groups && regexMatchIsSectionHeading.groups.headingText;
+        if (headingText) return headingText.trim() + '\n';
+    }
+
+    return '';
+}
+
+export async function read_line(filename: string, lineCallback: (s: string)=>string): Promise<string> {
+  const file = await Deno.open(filename);
+  const bufReader = new BufReader(file);
+  let sAccumulator = '';
+  let readlineResult: ReadLineResult | Deno.EOF;
+
+  while ((readlineResult = await bufReader.readLine()) !== Deno.EOF) {
+    const sCurrentLine = decoder.decode(readlineResult.line);
+    sAccumulator += lineCallback(sCurrentLine);
+  }
+
+  file.close();
+  return sAccumulator;
+}
+
+const sOut = await read_line("../section-127-effects.tex", handleLine);
 await Deno.writeFile("../section-127-effects.txt", encoder.encode(sOut));
+
+export {}
