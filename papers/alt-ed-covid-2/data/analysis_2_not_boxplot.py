5fb4d267c26e832db2f6bc6ceddab7c0cd00b5ff
John Vandivier <john@afterecon.com>
2021-04-21 22:08:44 -0400

    don't use boxplot

--- papers/alt-ed-covid-2/data/analysis_2_not_boxplot.py
+++ papers/alt-ed-covid-2/data/analysis_2_not_boxplot.py
@@ -7,5 +7,6 @@ df = GetVars.getData()
 
 # ref: https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.boxplot.html
 # ref: https://matplotlib.org/3.3.3/gallery/pyplots/boxplot_demo_pyplot.html
-plt.boxplot(df.favor_alt_creds, vert=False)
+# plt.boxplot(df.favor_alt_creds, vert=False)
+df.favor_alt_creds.hist()
 plt.show()
