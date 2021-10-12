35e9c32c4270324f8d6a5691a1595f6e88188ff3
John Vandivier <john@afterecon.com>
2021-10-12 13:57:14 -0400

    feat: four-way interaction

--- papers/alt-ed-gender/data/analysis_2_exploratory.py
+++ papers/alt-ed-gender/data/analysis_2_exploratory.py
@@ -8,28 +8,23 @@ deskewed = analysis.getDeskewedData()
 left_of_skew = analysis.getLowHirabilityGroup()
 
 print('\n')
+print("skewed data skew test:")
 skew_test_result = stats.skewtest(skewed.hirability)
 print(skew_test_result)
-print("skewed data is skewed")
 
 print('\n')
+print("deskewed data skew test:")
 deskewed_test_result = stats.skewtest(deskewed.hirability)
 print(deskewed_test_result)
-print("deskewed data is still skewed")
 
 print('\n')
-if len(left_of_skew) > 7:
-    left_of_skew_test_result = stats.skewtest(left_of_skew.hirability)
-    print(left_of_skew_test_result)
-else:
-    print("skew test failed due to insufficient sample size")
-
+print("left_of_skew fails skew test due to insufficient sample size")
 
 print("\n")
 deskewed_male = deskewed[deskewed.gender == "Male"]
 deskewed_female = deskewed[deskewed.gender == "Female"]
 # are men and women naively different?
-# p ~=.5 therefore currently retain null hypothesis of no difference
+# p ~=.68 therefore currently retain null hypothesis of no difference
 ttest, pval = stats.ttest_ind(deskewed_male.hirability, deskewed_female.hirability)
 print("hirability mean diff test by gender: " + str(pval))
 
@@ -39,38 +34,43 @@ deskewed_male_with_favor_programming_career = deskewed_male[deskewed_male.favor_
 deskewed_female_with_favor_programming_career = deskewed_female[deskewed_female.favor_programming_career > 0]
 n_favor_programming_career = len(deskewed_male_with_favor_programming_career) + len(deskewed_female_with_favor_programming_career)
 
+print("sample size for favor_programming_career: " + str(n_favor_programming_career))
+
 ttest, pval = stats.ttest_ind(deskewed_male_with_favor_programming_career.favor_programming_career,
     deskewed_female_with_favor_programming_career.favor_programming_career)
 
 print("favor_programming_career mean diff test by gender: " + str(pval))
-print("sample size for favor_programming_career: " + str(n_favor_programming_career))
 
 
 ## below analysis using deskewed data
 
-
 # industry alone
-# r2 .21, ar2 .31
+# ar2 .10, r2 .19, n 105, AIC 339
 m1 = '''hirability ~
     + industry
     + 1'''
 
 # three-way interaction alone is highly significant and better than industry
-# r2 .43, ar2 .39
+# ar2 .18, r2 .24, n 99, AIC 306
 m2 = '''hirability ~
     + gender*favor_programming_career*favor_seeking_risk
     + 1'''
 
 # industry and three-way interaction are substantially independent
 # ref: https://stats.stackexchange.com/a/9174/142294
-# r2 .599, ar2 .514
+# ar2 .25, r2 .38, n 99, AIC 308
 m3 = '''hirability ~
     + gender*favor_programming_career*favor_seeking_risk
     + industry
     + 1'''
 
+# this model is def overfit, but
+# four way interaction is meaningful in the average case after penalty (ar2 up)
+# could be a good backward selection or lasso starting point
+# ref: https://stats.stackexchange.com/a/9174/142294
+# ar2 .26, r2 .64, n 99, AIC 319
 m4 = '''hirability ~
-    + favor_programming_career
+    + gender*favor_programming_career*favor_seeking_risk*industry
     + 1'''
 
 
